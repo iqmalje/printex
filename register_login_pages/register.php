1db3fcedd01d22a6ad9e58d69f2d6d2bd46bdb0b
iqmalje <103345876+iqmalje@users.noreply.github.com>
2023-06-18 03:13:23 +0800

    DAH SIAP REGISTER

--- register_login_pages/register.php
+++ register_login_pages/register.php
@@ -20,22 +20,26 @@
         echo 'no file';
     }
     else
-    {
+    {   
+
+        
+        
         if(mysqli_query($conn, $sql))
         {
             $last_id = mysqli_insert_id($conn);
     
     
             //set up profilepic
+            
             $array = explode('.', $_FILES['profilepic']['name']);
             $extension = end($array);
             $filename = $_FILES['profilepic']['name'];
             $temp = $_FILES['profilepic']['tmp_name'];
-            $destination_path = getcwd().DIRECTORY_SEPARATOR;
-            $target_path = $destination_path . "images/$last_id" . $extension;
+            $destination_path = dirname(dirname(__FILE__));
+            $target_path = $destination_path . "/images/profile/$last_id." . $extension;
             move_uploaded_file($temp, $target_path);
         
-            $sql = "UPDATE accounts SET profilepic=$target_path WHERE UserID=$last_id";
+            $sql = "UPDATE accounts SET profilepic='/images/$last_id.$extension' WHERE UserID=$last_id";
             mysqli_query($conn, $sql);
     
             //set up address
